23550f0ec40c84c51823dc70b52ec97f04ac8903
georgegiam <giamouridis22@gmail.com>
2025-05-12 18:20:44 +0100

    unaiuthoized page

--- src/components/Unauthorized.tsx
+++ src/components/Unauthorized.tsx
@@ -1,12 +1,21 @@
-import { Link } from "react-router-dom";
+import styles from "../css/Dashboard.module.css";
 
 function Unauthorized() {
   return (
     <>
-      <div className="container text-center mt-5">
-        <h5>Error 401</h5>
-        <p>You don't have access to this page.</p>
-        <Link to="../">Go back</Link>
+      <div className="container text-center" style={{ marginTop: "13%" }}>
+        <h1 style={{ fontSize: "100px" }}>401</h1>
+        <h4>Forbidden</h4>
+        <p>Access to this resource is denied.</p>
+        <button
+          className={`${styles.primaryButton} btn mt-2`}
+          onClick={() => {
+            window.history.back();
+            setTimeout(() => window.location.reload(), 100);
+          }}
+        >
+          Go back
+        </button>
       </div>
     </>
   );
