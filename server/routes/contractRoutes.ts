a308b2ae79ff7e606e39cd74cc0c676bdd374a78
georgegiam <giamouridis22@gmail.com>
2025-10-06 12:21:24 +0100

    cotract type chenged

--- server/routes/contractRoutes.ts
+++ server/routes/contractRoutes.ts
@@ -122,7 +122,7 @@ router.post(
         client_optional_info: {
           consent_id: requestId,
         },
-        contract_type: "dsa",
+        contract_type: "consent_contract",
         validity_period: 0,
         notice_period: 0,
         contacts: {},
